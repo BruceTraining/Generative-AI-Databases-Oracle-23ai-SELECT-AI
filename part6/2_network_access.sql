7260972271a47af0a9636081a3a683f7a0a7581f
BruceTraining <b@btauthors.com>
2025-05-27 15:23:29 +0200

    Update 2_network_access.sql

--- part6/2_network_access.sql
+++ part6/2_network_access.sql
@@ -1,18 +1,18 @@
 -- run as ADMIN
 -- change the user permissions
 SET DEFINE OFF;
-GRANT RESOURCE TO user_oreilly_may25;
-GRANT CREATE SESSION TO user_oreilly_may25;
-GRANT CREATE VIEW TO user_oreilly_may25;
-GRANT CREATE TABLE TO user_oreilly_may25;
-GRANT CONNECT TO user_oreilly_may25;
-GRANT ALTER SYSTEM TO user_oreilly_may25;
-GRANT ALTER USER TO user_oreilly_may25;
-ALTER USER user_oreilly_may25 QUOTA 10M ON temp;
-GRANT CONSOLE_DEVELOPER TO user_oreilly_may25;
-GRANT DWROLE TO user_oreilly_may25;
-GRANT EXECUTE ON DBMS_CLOUD TO user_oreilly_may25;
-GRANT EXECUTE ON DBMS_CLOUD_AI TO user_oreilly_may25;
+GRANT RESOURCE TO user_oreilly_may27;
+GRANT CREATE SESSION TO user_oreilly_may27;
+GRANT CREATE VIEW TO user_oreilly_may27;
+GRANT CREATE TABLE TO user_oreilly_may27;
+GRANT CONNECT TO user_oreilly_may27;
+GRANT ALTER SYSTEM TO user_oreilly_may27;
+GRANT ALTER USER TO user_oreilly_may27;
+ALTER USER user_oreilly_may27 QUOTA 10M ON temp;
+GRANT CONSOLE_DEVELOPER TO user_oreilly_may27;
+GRANT DWROLE TO user_oreilly_may27;
+GRANT EXECUTE ON DBMS_CLOUD TO user_oreilly_may27;
+GRANT EXECUTE ON DBMS_CLOUD_AI TO user_oreilly_may27;
 
 
 
